ffb93865752a60182ab623f0fc643afedcc78a90
Arhan Busam <arhan.busam@gmail.com>
2021-06-04 18:25:04 +1000

    Headers in functions

--- demo1/demo1.py
+++ demo1/demo1.py
@@ -1,2 +1,26 @@
+# https://openpyxl.readthedocs.io/en/stable/
+from openpyxl import Workbook, load_workbook
+from .includes import *
+
+data = {
+    "ID": [1],
+    "URL": ["https://www.youtube.com/watch\?v\=6c2hT_f5Q2g"],
+    "Type": ["video+desc"],
+    "Created Date": [""],
+    "Download Date": [""],
+}
+
+filename = "videos.xlsx"
+
+
 def start():
-    print("demo1")
+    wb = Workbook()
+
+    ws = wb.active
+    ws.title = "Video List"
+    
+    headings = populate_headings()
+
+    ws.append(headings)
+
+    wb.save(filename=filename)
